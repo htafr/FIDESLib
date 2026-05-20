ecec7a38c8ccb6265badb2895fecb1fb9bad86d6
Keith Derrick <keith.derrick@lge.com>
2026-05-20 08:36:41 -0700

    Allow benchmarks to run as per v2.1.0

--- bench/Benchmark.cuh
+++ bench/Benchmark.cuh
@@ -34,7 +34,7 @@ extern std::vector<FIDESlib::PrimeRecord> p64;
 extern std::vector<FIDESlib::PrimeRecord> sp64;
 
 #define BATCH_CONFIG \
-	{ 100, 16384 }
+	{ 100 }
 
 #define LEVEL_CONFIG \
 	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 }
